1b336937e0378b0205e916bea3a76c6c7df4f6d6
overlookmotel <theoverlookmotel@gmail.com>
2020-02-22 11:23:25 +0000

    Tests: All `.use` tests share same `beforeEach` set-up [refactor]

--- test/use.test.js
+++ test/use.test.js
@@ -20,39 +20,73 @@ import './support/index.js';
 
 // TODO Tests for promise rejection/sync error in fetch function
 
-describe('factory.use', () => {
-	let factory, fetchFn, promise, resolve, isResolved, req,
-		resource, rendered, App, Component, container;
-	beforeEach(() => {
-		({promise, resolve, isResolved} = defer());
-		promise.abort = spy();
+let factory, fetchFn, req1, req2, setReq, promise1, promise2, resolve1, resolve2, isResolved1,
+	resource1, resource2, fetched2, rendered,
+	App, renderApp, Intermediate, Component, container;
+beforeEach(() => {
+	({promise: promise1, resolve: resolve1, isResolved: isResolved1} = defer());
+	({promise: promise2, resolve: resolve2} = defer());
 
-		fetchFn = awaitSpy(() => promise);
-		factory = createResourceFactory(fetchFn);
-		req = {};
+	promise1.abort = spy();
 
-		// `rendered` called when Component called with resolved resource
-		rendered = awaitSpy();
-
-		Component = spy(({resResource}) => {
-			const res = resResource.read();
-			rendered(res);
-			// Return `.a` property of result if present, or null
-			return !res ? null : res.a || null;
-		});
-
-		App = spy(() => {
-			resource = factory.use(req);
-			return (
-				<Suspense fallback="Loading">
-					<Component resResource={resource} />
-				</Suspense>
-			);
-		});
-
-		container = render(<App />);
+	fetched2 = spy();
+	fetchFn = awaitSpy(({num}) => {
+		if (num === 1) return promise1;
+		fetched2();
+		return promise2;
 	});
 
+	factory = createResourceFactory(fetchFn);
+
+	req1 = {num: 1};
+	req2 = {num: 2};
+
+	// `rendered` called when Component called with resolved resource
+	rendered = awaitSpy();
+
+	Component = awaitSpy(({resource}) => {
+		const res = resource.read();
+		rendered(res);
+		// Return `.a` property of result if present, or null
+		return !res ? null : res.a || null;
+	});
+
+	Intermediate = spy(({req}) => {
+		const resource = factory.use(req);
+		if (req.num === 1) {
+			resource1 = resource;
+		} else {
+			resource2 = resource;
+		}
+
+		return (
+			<Suspense fallback="Loading">
+				<Component resource={resource} />
+			</Suspense>
+		);
+	});
+
+	App = class extends React.Component {
+		constructor(props) {
+			super(props);
+			this.state = {req: req1};
+
+			setReq = req => this.setState({req});
+		}
+	};
+
+	renderApp = spy(function() {
+		const {req} = this.state; // eslint-disable-line no-invalid-this
+		if (!req) return 'Nothing';
+		return <Intermediate req={req} />;
+	});
+
+	App.prototype.render = renderApp;
+
+	container = render(<App />);
+});
+
+describe('factory.use', () => {
 	// Clean-up - Wait for async processes to complete before test ends
 	// so any errors or unhandled rejections make test fail.
 	afterEach(async () => {
@@ -60,8 +94,8 @@ describe('factory.use', () => {
 		await fetchFn.calledOnce();
 
 		// If promise was resolved, wait for render to complete before test ends
-		if (isResolved()) {
-			await promise;
+		if (isResolved1()) {
+			await promise1;
 			await rendered.calledOnce();
 			await tick();
 		}
@@ -72,8 +106,8 @@ describe('factory.use', () => {
 	});
 
 	it('returns a resource', () => {
-		expect(resource).toBeObject();
-		expect(isResource(resource)).toBeTrue();
+		expect(resource1).toBeObject();
+		expect(isResource(resource1)).toBeTrue();
 	});
 
 	it('does not call fetch function synchronously', () => {
@@ -83,13 +117,13 @@ describe('factory.use', () => {
 	it('calls fetch function asynchronously with request', async () => {
 		await fetchFn.calledOnce();
 		expect(fetchFn).toHaveBeenCalledTimes(1);
-		expect(fetchFn).toHaveBeenCalledWith(req);
+		expect(fetchFn).toHaveBeenCalledWith(req1);
 	});
 
 	it('does not call fetch function again when fetch promise resolves', async () => {
 		await fetchFn.calledOnce();
-		resolve();
-		await promise;
+		resolve1();
+		await promise1;
 		await rendered.calledOnce();
 		await tick();
 		expect(fetchFn).toHaveBeenCalledTimes(1);
@@ -97,18 +131,18 @@ describe('factory.use', () => {
 
 	describe('resource.read', () => {
 		it('is a function', () => {
-			expect(resource.read).toBeFunction();
+			expect(resource1.read).toBeFunction();
 		});
 
 		describe('before fetch function called', () => {
 			it('throws promise', () => {
-				const thrown = tryCatch(() => resource.read());
+				const thrown = tryCatch(() => resource1.read());
 				expect(fetchFn).not.toHaveBeenCalled();
 				expect(thrown).toBeInstanceOf(Promise);
 			});
 
 			it('root rendered once only', () => {
-				expect(App).toHaveBeenCalledTimes(1);
+				expect(renderApp).toHaveBeenCalledTimes(1);
 			});
 
 			it('component rendered once only', () => {
@@ -124,30 +158,30 @@ describe('factory.use', () => {
 		describe('before fetch promise resolves', () => {
 			it('throws promise', async () => {
 				await fetchFn.calledOnce();
-				const thrown = tryCatch(() => resource.read());
+				const thrown = tryCatch(() => resource1.read());
 				expect(thrown).toBeInstanceOf(Promise);
 			});
 
 			it('throws same promise as before fetch function called', async () => {
-				const thrownBefore = tryCatch(() => resource.read());
+				const thrownBefore = tryCatch(() => resource1.read());
 				expect(fetchFn).not.toHaveBeenCalled();
 
 				await fetchFn.calledOnce();
-				const thrownAfter = tryCatch(() => resource.read());
+				const thrownAfter = tryCatch(() => resource1.read());
 
 				expect(thrownAfter).toBe(thrownBefore);
 			});
 
 			it('throws different promise from promise returned by fetch function', async () => {
 				await fetchFn.calledOnce();
-				const thrown = tryCatch(() => resource.read());
-				expect(thrown).not.toBe(promise);
+				const thrown = tryCatch(() => resource1.read());
+				expect(thrown).not.toBe(promise1);
 			});
 
 			it('root not re-rendered', async () => {
 				await fetchFn.calledOnce();
 				await tick();
-				expect(App).toHaveBeenCalledTimes(1);
+				expect(renderApp).toHaveBeenCalledTimes(1);
 			});
 
 			it('component not re-rendered', async () => {
@@ -167,19 +201,19 @@ describe('factory.use', () => {
 			beforeEach(async () => {
 				ret = {a: 'abc'};
 				await fetchFn.calledOnce();
-				resolve(ret);
-				await promise;
+				resolve1(ret);
+				await promise1;
 			});
 
 			it('returns fetch promise resolution value', async () => {
-				const readRet = resource.read();
+				const readRet = resource1.read();
 				expect(readRet).toBe(ret);
 			});
 
 			it('root not re-rendered', async () => {
 				await rendered.calledOnce();
 				await tick();
-				expect(App).toHaveBeenCalledTimes(1);
+				expect(renderApp).toHaveBeenCalledTimes(1);
 			});
 
 			it('component re-rendered once only', async () => {
@@ -201,24 +235,24 @@ describe('factory.use', () => {
 		describe('called before fetch function called', () => {
 			afterEach(() => {
 				// Prevent parent `afterEach` hook waiting for fetch function to be called
-				fetchFn();
+				fetchFn(req1);
 			});
 
 			it('prevents fetch function being called', async () => {
-				resource.dispose();
+				resource1.dispose();
 				await tick();
 
 				expect(fetchFn).not.toHaveBeenCalled();
 			});
 
 			it('leaves promise thrown by `.read()` forever pending', async () => {
-				const thrown = tryCatch(() => resource.read());
+				const thrown = tryCatch(() => resource1.read());
 				const thenSpy = spy();
 				thrown.then(thenSpy, thenSpy);
 
-				resource.dispose();
-				resolve();
-				await promise;
+				resource1.dispose();
+				resolve1();
+				await promise1;
 				await tick();
 
 				expect(thenSpy).not.toHaveBeenCalled();
@@ -234,9 +268,9 @@ describe('factory.use', () => {
 			});
 
 			async function disposeThenResolve() {
-				resource.dispose();
-				resolve();
-				await promise;
+				resource1.dispose();
+				resolve1();
+				await promise1;
 				await tick();
 
 				// Prevent `afterEach` hook waiting for component to be called
@@ -244,13 +278,13 @@ describe('factory.use', () => {
 			}
 
 			it('calls `promise.abort()`', async () => {
-				expect(promise.abort).not.toHaveBeenCalled();
-				resource.dispose();
-				expect(promise.abort).toHaveBeenCalledTimes(1);
+				expect(promise1.abort).not.toHaveBeenCalled();
+				resource1.dispose();
+				expect(promise1.abort).toHaveBeenCalledTimes(1);
 			});
 
 			it('leaves promise thrown by `.read()` forever pending', async () => {
-				const thrown = tryCatch(() => resource.read());
+				const thrown = tryCatch(() => resource1.read());
 				const thenSpy = spy();
 				thrown.then(thenSpy, thenSpy);
 
@@ -260,9 +294,9 @@ describe('factory.use', () => {
 			});
 
 			it('root not re-rendered', async () => {
-				expect(App).toHaveBeenCalledTimes(1);
+				expect(renderApp).toHaveBeenCalledTimes(1);
 				await disposeThenResolve();
-				expect(App).toHaveBeenCalledTimes(1);
+				expect(renderApp).toHaveBeenCalledTimes(1);
 			});
 
 			it('component not re-rendered', async () => {
@@ -280,25 +314,25 @@ describe('factory.use', () => {
 		describe('called after fetch promise resolves', () => {
 			beforeEach(async () => {
 				await fetchFn.calledOnce();
-				resolve({a: 'abc'});
-				await promise;
+				resolve1({a: 'abc'});
+				await promise1;
 			});
 
 			it('does not call `promise.abort()`', async () => {
-				resource.dispose();
-				expect(promise.abort).not.toHaveBeenCalled();
+				resource1.dispose();
+				expect(promise1.abort).not.toHaveBeenCalled();
 			});
 
 			it('root not re-rendered', async () => {
 				await rendered.calledOnce();
 				await tick();
 
-				expect(App).toHaveBeenCalledTimes(1);
+				expect(renderApp).toHaveBeenCalledTimes(1);
 
-				resource.dispose();
+				resource1.dispose();
 				await tick();
 
-				expect(App).toHaveBeenCalledTimes(1);
+				expect(renderApp).toHaveBeenCalledTimes(1);
 			});
 
 			it('component not re-rendered', async () => {
@@ -307,14 +341,14 @@ describe('factory.use', () => {
 
 				expect(Component).toHaveBeenCalledTimes(2);
 
-				resource.dispose();
+				resource1.dispose();
 				await tick();
 
 				expect(Component).toHaveBeenCalledTimes(2);
 			});
 
 			it('leaves content rendered', async () => {
-				resource.dispose();
+				resource1.dispose();
 
 				await rendered.calledOnce();
 				await tick();
@@ -326,72 +360,6 @@ describe('factory.use', () => {
 });
 
 describe('auto-disposal', () => {
-	let factory, fetchFn, req1, req2, setReq, promise1, promise2, resolve1, resolve2,
-		resource1, resource2, fetched2, rendered,
-		App, renderApp, Intermediate, Component, container;
-	beforeEach(() => {
-		({promise: promise1, resolve: resolve1} = defer());
-		({promise: promise2, resolve: resolve2} = defer());
-
-		promise1.abort = spy();
-
-		fetched2 = spy();
-		fetchFn = awaitSpy(({num}) => {
-			if (num === 1) return promise1;
-			fetched2();
-			return promise2;
-		});
-
-		factory = createResourceFactory(fetchFn);
-
-		req1 = {num: 1};
-		req2 = {num: 2};
-
-		// `rendered` called when Component called with resolved resource
-		rendered = awaitSpy();
-
-		Component = awaitSpy(({resource}) => {
-			const res = resource.read();
-			rendered(res);
-			// Return `.a` property of result if present, or null
-			return !res ? null : res.a || null;
-		});
-
-		Intermediate = spy(({req}) => {
-			const resource = factory.use(req);
-			if (req.num === 1) {
-				resource1 = resource;
-			} else {
-				resource2 = resource;
-			}
-
-			return (
-				<Suspense fallback="Loading">
-					<Component resource={resource} />
-				</Suspense>
-			);
-		});
-
-		App = class extends React.Component {
-			constructor(props) {
-				super(props);
-				this.state = {req: req1};
-
-				setReq = req => this.setState({req});
-			}
-		};
-
-		renderApp = spy(function() {
-			const {req} = this.state; // eslint-disable-line no-invalid-this
-			if (!req) return 'Nothing';
-			return <Intermediate req={req} />;
-		});
-
-		App.prototype.render = renderApp;
-
-		container = render(<App />);
-	});
-
 	describe('when request changes', () => {
 		describe('before fetch function called', () => {
 			beforeEach(async () => {
